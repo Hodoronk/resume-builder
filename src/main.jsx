ca7709393f74d1fdfcc34f34e2c57b3dd914acfc
Hodoronk <tanasa.andrei@hotmail.com>
2024-01-20 21:17:21 +0200

    rough draft of main components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ClearOrLoad from './ClearOrLoad.jsx'
+import GeneralInfo from './GeneralInfo.jsx'
+import Education from './Education.jsx'
+import Experience from './Experience.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClearOrLoad />
+    <GeneralInfo />
+    <Education />
+    <Experience />
   </React.StrictMode>,
 )
